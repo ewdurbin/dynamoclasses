30d8b09cb298c57fa69a63ac04e98a9ed60be058
Ernest W. Durbin III <ewdurbin@gmail.com>
2018-04-03 15:44:26 -0400

    get/save!

--- dynamoclasses/__init__.py
+++ dynamoclasses/__init__.py
@@ -1,28 +1,112 @@
 
-from dataclasses import dataclass
+from dataclasses import dataclass, asdict
+
+import boto3
 
 
-def _process_class(cls, *, table_name, partition_key, sort_key, data_class_kwargs):
+TYPE_MAPPING = {
+    str: {"key": "S", "fn": lambda x: str(x)},
+    float: {"key": "N", "fn": lambda x: str(x)},
+    int: {"key": "N", "fn": lambda x: str(x)},
+    bytes: {"key": "B", "fn": lambda x: str(x)},
+    dict: {"key": "M", "fn": lambda x: str(x)},
+    bool: {"key": "BOOL", "fn": lambda x: x},
+    None: {"key": "NULL", "fn": lambda x: x},
+}
+
+
+def _process_class(
+    cls, *, table_name, partition_key_name, sort_key_name, data_class_kwargs
+):
 
     data_class = dataclass(cls, **data_class_kwargs)
+
+    if partition_key_name not in data_class.__dataclass_fields__:
+        raise Exception(
+            f'Partition Key Name "{partition_key_name}" not found in class fields'
+        )
+
+    if sort_key_name is not None and sort_key_name not in data_class.__dataclass_fields__:
+        raise Exception(f'Sort Key Name "{sort_key_name}" not found in class fields')
 
     setattr(
         data_class,
         "__dynamoclass_params__",
         {
             "table_name": table_name,
-            "partition_key": partition_key,
-            "sort_key": sort_key,
+            "partition_key_name": partition_key_name,
+            "sort_key_name": sort_key_name,
         },
     )
 
-    def save(cls):
-        return f"saving: partition_key={cls.__dynamoclass_params__['partition_key']} sort_key={cls.__dynamoclass_params__['sort_key']} table_name={cls.__dynamoclass_params__['table_name']}"
+    setattr(data_class, "__dynamoclass_client__", boto3.client("dynamodb"))
+
+    def _format_to_dynamo(self):
+        return {
+            k: {TYPE_MAPPING[type(v)]["key"]: TYPE_MAPPING[type(v)]["fn"](v)}
+            for k, v in asdict(self).items()
+        }
+
+    def save(self):
+        return self.__dynamoclass_client__.put_item(
+            TableName=self.__dynamoclass_params__["table_name"],
+            Item=self._format_to_dynamo(),
+        )
+
+    @classmethod
+    def _format_to_dataclass(cls, dynamodb_item):
+        kwargs = {}
+        for field_name, value in dynamodb_item.items():
+            if field_name not in cls.__dataclass_fields__:
+                raise ValueError(
+                    f"Cannot render field with name {field_name}! "
+                    f"No such field name found for {cls}!"
+                )
+
+            kwargs[field_name] = cls.__dataclass_fields__[field_name].type(
+                list(value.values())[0]
+            )
+        return kwargs
+
+    @classmethod
+    def _render_to_type(cls, field_name, value):
+        if field_name not in cls.__dataclass_fields__:
+            raise ValueError(
+                f"Cannot render field with name {field_name}! "
+                f"No such field name found for {cls}!"
+            )
+
+        return {
+            TYPE_MAPPING[cls.__dataclass_fields__[field_name].type][
+                "key"
+            ]: TYPE_MAPPING[
+                cls.__dataclass_fields__[field_name].type
+            ][
+                "fn"
+            ](
+                cls.__dataclass_fields__[field_name].type(value)
+            )
+        }
 
     @classmethod
     def get(cls, *, partition_key, sort_key):
-        return f"fetching: partition_key={partition_key} sort_key={sort_key} table_name={table_name}"
+        key = {
+            cls.__dynamoclass_params__["partition_key_name"]: cls._render_to_type(
+                cls.__dynamoclass_params__["partition_key_name"], partition_key
+            )
+        }
+        if cls.__dynamoclass_params__["sort_key_name"] is not None:
+            key[cls.__dynamoclass_params__["sort_key_name"]] = cls._render_to_type(
+                cls.__dynamoclass_params__["sort_key_name"], sort_key
+            )
+        item = cls.__dynamoclass_client__.get_item(
+            TableName=cls.__dynamoclass_params__["table_name"], Key=key
+        )
+        return cls(**cls._format_to_dataclass(item["Item"]))
 
+    data_class._format_to_dynamo = _format_to_dynamo
+    data_class._format_to_dataclass = _format_to_dataclass
+    data_class._render_to_type = _render_to_type
     data_class.save = save
     data_class.get = get
 
@@ -30,15 +114,15 @@ def _process_class(cls, *, table_name, partition_key, sort_key, data_class_kwarg
 
 
 def dynamoclass(
-    _cls=None, *, table_name=None, partition_key=None, sort_key=None, **kwargs
+    _cls=None, *, table_name=None, partition_key_name=None, sort_key_name=None, **kwargs
 ):
 
     def wrap(cls):
         return _process_class(
             cls,
             table_name=table_name,
-            partition_key=partition_key,
-            sort_key=sort_key,
+            partition_key_name=partition_key_name,
+            sort_key_name=sort_key_name,
             data_class_kwargs=kwargs,
         )
 
--- dynamoclasses/test/test_dynamoclasses.py
+++ dynamoclasses/test/test_dynamoclasses.py
@@ -1,36 +1,91 @@
 import unittest
 
-
 from dynamoclasses import dynamoclass
+
+import botocore
+
+from moto import mock_dynamodb2
 
 
 class DynamoClassTestCase(unittest.TestCase):
 
+    @classmethod
+    def setUpClass(cls):
+        cls.mock = mock_dynamodb2()
+        cls.mock.start()
+        cls.session = botocore.session.get_session()
+        cls.client = cls.session.create_client("dynamodb")
+        cls.client.create_table(
+            TableName="inventory",
+            KeySchema=[{"AttributeName": "item_id", "KeyType": "HASH"}],
+            AttributeDefinitions=[{"AttributeName": "item_id", "AttributeType": "S"}],
+            ProvisionedThroughput={"ReadCapacityUnits": 1, "WriteCapacityUnits": 1},
+        )
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.mock.stop()
+
     def test_init(self):
 
-        @dynamoclass
-        class InventoryItem:
-            name: str
+        with self.assertRaises(Exception):
 
-        item = InventoryItem("hammers")
-        self.assertEqual(item.name, "hammers")
+            @dynamoclass
+            class InventoryItem:
+                name: str
+
+    def test_init_options(self):
+
+        with self.assertRaises(Exception):
+
+            @dynamoclass(
+                table_name="inventory",
+                partition_key_name="partition",
+                sort_key_name="sort",
+            )
+            class InventoryItem:
+                name: str
+
+    def test_init_with_valid_options(self):
+
+        @dynamoclass(table_name="inventory", partition_key_name="item_id")
+        class InventoryItem:
+            item_id: str
+
+        item = InventoryItem(item_id="000011123")
+        self.assertEqual(
+            item.__dynamoclass_params__,
+            {
+                "table_name": "inventory",
+                "partition_key_name": "item_id",
+                "sort_key_name": None,
+            },
+        )
 
     def test_save(self):
 
-        @dynamoclass(table_name="inventory", partition_key="partition", sort_key="sort")
+        @dynamoclass(table_name="inventory", partition_key_name="item_id")
         class InventoryItem:
-            name: str
+            item_id: str
 
-        item = InventoryItem("hammers")
-        save_string = item.save()
-        self.assertEqual(
-            save_string,
-            "saving: partition_key=partition sort_key=sort table_name=inventory",
+        item = InventoryItem(item_id="hammers")
+        item.save()
+        dynamo_item = self.client.get_item(
+            TableName="inventory", Key={"item_id": {"S": "hammers"}}
         )
-        get_string = item.get(partition_key="foo", sort_key="bar")
-        self.assertEqual(
-            get_string, "fetching: partition_key=foo sort_key=bar table_name=inventory"
+        self.assertEqual({"item_id": {"S": "hammers"}}, dynamo_item["Item"])
+
+    def test_get(self):
+
+        @dynamoclass(table_name="inventory", partition_key_name="item_id")
+        class InventoryItem:
+            item_id: str
+
+        dynamo_item = self.client.put_item(
+            TableName="inventory", Item={"item_id": {"S": "nails"}}
         )
+        item = InventoryItem.get(partition_key="nails", sort_key=None)
+        self.assertEqual("nails", item.item_id)
 
 
 if __name__ == "__main__":
